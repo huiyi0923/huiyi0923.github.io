1db6e2122aaa62d49c1261d7535fa1b48427f533
huiyi0923 <126973386+huiyi0923@users.noreply.github.com>
2023-08-02 20:33:56 +0800

    Add files via upload

---
+++ newweb/MY.js
@@ -0,0 +1,36 @@
+wdzs=12610
+wdzs2=1326310
+wdzs3=24697
+document.querySelector('.zhaop .zhaop2 span').addEventListener('click',()=>{
+    document.querySelector('.yc').style.display='block'
+})
+document.querySelector('.ckyy').addEventListener('click',()=>{
+    document.querySelector('.yc2').style.display='block'
+})
+document.querySelector('#gban2').addEventListener('click',()=>{
+    document.querySelector('.yc').style.display='none'
+})
+document.querySelector('#gban3').addEventListener('click',()=>{
+    document.querySelector('.yc2').style.display='none'
+})
+document.querySelector('.zan2').addEventListener('click',()=>{
+    alert('点赞成功')
+    document.querySelector('.zs').innerHTML=`${wdzs+1}`
+    wdzs++
+})
+document.querySelector('.zan').addEventListener('click',()=>{
+    alert('点赞成功')
+    document.querySelector('.zs2').innerHTML=`${wdzs2+1}`
+    wdzs2++
+})
+document.querySelector('.zan3').addEventListener('click',()=>{
+    alert('点赞成功')
+    document.querySelector('.zs3').innerHTML=`${wdzs3+1}`
+    wdzs3++
+})
+document.querySelector('.ewm svg').addEventListener('click',()=>{
+    document.querySelector('.yc3').style.display='block'
+})
+document.querySelector('#gban1').addEventListener('click',()=>{
+    document.querySelector('.yc3').style.display='none'
+})
