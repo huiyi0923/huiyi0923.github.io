8ebd2baa362d1214ad50ebbeea486a26925ed629
huiyi0923 <126973386+huiyi0923@users.noreply.github.com>
2023-08-05 17:17:06 +0800

    Add files via upload

---
+++ data/userdata/cs.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    11111111111
+    <?php
+    $a='111'
+    echo 'xx';
+    echo $a;
+?>
+</body>
+</html>
