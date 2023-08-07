e500955c34be0fc7ad7847f004f0b81c4d718b45
huiyi0923 <126973386+huiyi0923@users.noreply.github.com>
2023-08-07 21:08:29 +0800

    Add files via upload

--- newweb/jiam.js
+++ newweb/jiam.js
@@ -89,3 +89,19 @@ svg1.style.color='#000'
 bl=0
 }
 })
+function show_runtime() {
+        window.setTimeout("show_runtime()", 1000);
+        X = new Date("8/6/2023 22:10:59");
+        Y = new Date();
+        T = (Y.getTime() - X.getTime());
+        M = 24 * 60 * 60 * 1000;
+        a = T / M;
+        A = Math.floor(a);
+        b = (a - A) * 24;
+        B = Math.floor(b);
+        c = (b - B) * 60;
+        C = Math.floor((b - B) * 60);
+        D = Math.floor((c - C) * 60);
+        document.querySelector(".webtime").innerHTML = "网站已运行了: " + A + "天" + B + "小时" + C + "分" + D + "秒"
+    }
+show_runtime();
